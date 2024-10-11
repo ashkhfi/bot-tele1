3bf43ba9316e860ed51b07479c560954c6e6c196
Ashabul Kahfi <ashkhfi.dev@gmail.com>
2024-10-11 11:34:32 +0700

    update chart

--- tes.py
+++ tes.py
@@ -1,42 +1,13 @@
-import psycopg2
-def connect_to_postgres():
-    try:
-        conn = psycopg2.connect(
-            user="postgres",  # Ganti dengan username PostgreSQL
-            password="Solo987@!",  # Ganti dengan password PostgreSQL
-            host="1.tcp.ap.ngrok.io",  # Ganti dengan host yang benar
-            port="21674",  # Ganti dengan port yang benar
-            database="npmcj"  # Ganti dengan nama database
-        )
-        return conn
-    except Exception as e:
-        print(f"Gagal terhubung ke database: {e}")
-        return None
-
+from config import connect_to_postgres
+from data_handler import get_data_chart
+from chart_system import plot_data
 
 conn = connect_to_postgres()
-def execute_query(conn, query, params=None):
-    """Eksekusi query SQL dan kembalikan hasilnya."""
-    try:
-        with conn.cursor() as cursor:
-            cursor.execute(query, params)
-            if cursor.description:  # Jika query menghasilkan hasil (SELECT)
-                return cursor.fetchall()
-            else:
-                conn.commit()  # Jika bukan SELECT, commit perubahan
-    except Exception as e:
-        print(f"Error saat mengeksekusi query: {e}")
-
-    
 if conn:
-        # Contoh query untuk mengambil data
-      select_query = "SELECT * FROM magang_dataset;"  # Ganti dengan nama tabel Anda
-      results = execute_query(conn, select_query)
-        
-      if results:
-          for row in results:
-              print(row)  # Cetak hasilnya
-        
-        # Jangan lupa menutup koneksi
-      conn.close()
-      print("Koneksi ke database ditutup.")
+    df = get_data_chart(conn, 'lte_tampe_pl', 20)  # Mendapatkan data 7 hari terakhir
+    if df.empty:
+        print("Tidak ada data yang ditemukan.")
+    else:
+        plot_data(df, 'eut', 'availability')  # Menampilkan chart
+else:
+    print("Terjadi kesalahan koneksi")
